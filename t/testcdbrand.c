e233481844bc3cbfaf9b549aa52720e9b1cd677f
Glenn Strauss <gs-github.com@gluelogic.com>
2011-08-23 13:55:15 +0000

    commit some code used for performance comparisons (documentation of results will be forthcoming)

---
+++ t/testcdbrand.c
@@ -0,0 +1,71 @@
+/* 
+ $ CDB_BUILD_PATH=/path/to/build/of/cdb-0.75
+ $ gcc -pipe -Wall -Winline -pedantic -ansi -std=c99 -D_THREAD_SAFE -O3 -g -o testcdbrand -I $CDB_BUILD_PATH testcdbrand.c $CDB_BUILD_PATH/cdb.a $CDB_BUILD_PATH/[abu]*.a
+ */
+
+#ifndef _POSIX_C_SOURCE
+#define _POSIX_C_SOURCE 200112L
+#endif
+#ifndef _XOPEN_SOURCE /* IOV_MAX */
+#define _XOPEN_SOURCE 500
+#endif
+
+/* large file support needed for open() input file > 2 GB */
+#if 0
+#if defined(_AIX)
+#ifndef _LARGE_FILES
+#define _LARGE_FILES
+#endif
+#else /*#elif defined(__linux) || defined(__sun) || defined(__hpux)*/
+#ifndef _FILE_OFFSET_BITS
+#define _FILE_OFFSET_BITS 64
+#endif
+#ifndef _LARGEFILE_SOURCE
+#define _LARGEFILE_SOURCE 1
+#endif
+#ifndef _LARGEFILE64_SOURCE
+#define _LARGEFILE64_SOURCE 1
+#endif
+#endif
+#endif
+
+#include <sys/mman.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "cdb.h"
+
+int main (int argc, char *argv[])
+{
+    const char *p;
+    const char *end;
+    struct cdb c;
+    struct stat st;
+    int fd;
+    const unsigned int klen = 8;
+    /* input stream must have keys of constant len 8 */
+
+    if (argc < 3) return -1;
+
+    /* open mcdb */
+    if ((fd = open(argv[1], O_RDONLY, 0777)) == -1) {perror("open"); return -1;}
+    memset(&c, '\0', sizeof(struct cdb));
+    cdb_init(&c, fd);
+
+    /* open input file */
+    if ((fd = open(argv[2], O_RDONLY, 0777)) == -1) {perror("open"); return -1;}
+    if (fstat(fd, &st) != 0)                        {perror("fstat");return -1;}
+    p = (const char *)mmap(0, st.st_size, PROT_READ, MAP_SHARED, fd, 0);
+    if (p == MAP_FAILED)                            {perror("mmap"); return -1;}
+    close(fd);
+
+    /* read each key from input mmap and query mcdb
+     * (no error checking since key might not exist) */
+    for (end = p+st.st_size; p < end; p += klen)
+        cdb_find(&c, p, klen);
+    return 0;
+}
