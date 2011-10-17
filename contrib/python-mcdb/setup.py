cd0f9df2f9f600d677196172adb87692b47714be
Glenn Strauss <gs-github.com@gluelogic.com>
2011-10-16 22:54:18 -0400

    build contrib/* modules against static libmcdb.a
    build contrib/* against static libmcdb.a for standalone enjoyment.
    See contrib/README for additional notes.

--- contrib/python-mcdb/setup.py
+++ contrib/python-mcdb/setup.py
@@ -1,17 +1,27 @@
 #! /usr/bin/env python
 
-SRCDIR   = "src"
-SRCFILES = map(lambda f: SRCDIR + '/' + f + '.c', ["mcdbpy"])
-
 from distutils.core import setup, Extension
 
-setup (# Distribution meta-data
+classifiers = [
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    ('License :: OSI Approved :: GNU Library or Lesser General Public '
+     'License (LGPL)'),
+    'Operating System :: POSIX',
+    'Operating System :: Unix',
+    'Programming Language :: Python',
+    'Topic :: Database',
+    'Topic :: Software Development :: Libraries :: Python Modules']
+
+setup (
         name = "python-mcdb",
         version = "0.01",
         description = "Interface to mcdb constant database files",
+        url = "https://github.com/gstrauss/mcdb/",
         author = "gstrauss",
         author_email = "code()gluelogic.com",
-	license = "GPL",
+        license = "LGPL",
+        classifiers = classifiers,
         long_description = \
 '''Python interface to create and read mcdb constant databases.
 
@@ -19,7 +29,15 @@ The python-mcdb extension module wraps interfaces in libmcdb.so
 See https://github.com/gstrauss/mcdb/ for latest info on mcdb.
 mcdb is based on D. J. Bernstein's constant database package
 (see http://cr.yp.to/cdb.html).''',
-        ext_modules = [ Extension( "mcdb", SRCFILES, libraries=['mcdb']) ],
-        url = "https://github.com/gstrauss/mcdb/",
+        platforms = 'POSIX, Unix',
+        ext_modules = [ Extension( "mcdb",
+                                   ["src/mcdbpy.c"],
+                                   # compile,link with local, static libmcdb.a
+                                   include_dirs=["../../.."],
+                                   extra_link_args=["../../libmcdb.a",
+                                     "-Wl,--version-script,src/pythonext.map"],
+                                   # compile,link with system libmcdb.so
+                                   #libraries=['mcdb']
+                                 )
+                      ],
       )
-
--- contrib/ruby-mcdb/extconf.rb
+++ contrib/ruby-mcdb/extconf.rb
@@ -1,7 +1,12 @@
 require 'mkmf'
 
+# compile and link against local, static libmcdb.a
+$CPPFLAGS += ' -I ../../..'
+$LDFLAGS  += ' ../../libmcdb.a'
+$LDFLAGS  += ' -Wl,--version-script,rubyext.map'
+
 have_library('mcdb')
-$CFLAGS += ' -std=c99'
+$CPPFLAGS += ' -std=c99'
 if RUBY_VERSION =~ /\A1\.8/ then
   $CPPFLAGS += " -DRUBY_1_8_x"
 end
