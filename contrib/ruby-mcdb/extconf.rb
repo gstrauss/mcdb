c64fa3331752dcaac36bfcc7a8f0d3e0aba2c745
Glenn Strauss <gstrauss@gluelogic.com>
2021-01-17 20:42:23 -0500

    link with -load_hidden on MacOS (Darwin)
    link with -Wl,--version-script,... only on Linux (GNU ld)

--- contrib/ruby-mcdb/extconf.rb
+++ contrib/ruby-mcdb/extconf.rb
@@ -1,9 +1,16 @@
 require 'mkmf'
+require 'rbconfig'
+
+case RbConfig::CONFIG['host_os']
+when /linux/
+  $LDFLAGS+= ' -Wl,--version-script,rubyext.map'
+when /darwin/
+  $LDFLAGS+= ' -load_hidden'
+end
 
 # compile and link against local, static libmcdb.a
 $CPPFLAGS += ' -I ../../..'
 $LDFLAGS  += ' ../../libmcdb.a'
-$LDFLAGS  += ' -Wl,--version-script,rubyext.map'
 
 have_library('mcdb')
 $CPPFLAGS += ' -std=c99'
